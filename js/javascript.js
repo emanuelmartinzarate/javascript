17a610160ea058ea2ad93289ce0ac258132b6556
Emanuel Martin Zarate <emanuelmartin@gmail.com>
2022-03-22 19:03:48 -0300

    se quita la funcion factorial dado que no pertenece al dominio del proyecto

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,3 @@
-/*Esta Funcion corresponde al primer desafio*/
-function factorizar(num){
-    let total = 1;
-    do{
-        num = parseInt(prompt("Ingrese un numero a fatorizar"));
-        
-        if(num>0){
-            for(let i=1;i<=num;i++){
-                console.log(total=total*i);
-            }
-        }else if(num === 0){
-            console.log(total);
-        }
-    }
-    while(num<0 || isNaN(num))
-}
-
 function enviar(){
     // Simulo la toma de info desde la pagina, luego sera reemplazado por el uso del DOM
     let nombre = "Emanuel Martin";
@@ -38,7 +21,7 @@ function carrito(){
 
     for(let i=0;i<cantidadProd;i++){
         if(cantidadProd){
-            total= calcularTotal(total,precioProd); 
+            total= calcularTotal(total,precioProd);
         }
     }
 
