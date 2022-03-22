4629c8dc5c28c37e3d34bd4ce25ef957dc17164d
Emanuel Martin Zarate <emanuelmartin@gmail.com>
2022-03-22 11:41:25 -0300

    agregado de funciones en archivo js

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,51 @@
-let total = 1;
-let num = 1;
-do{
-    num = parseInt(prompt("Ingrese un numero a fatorizar"));
-    
-    if(num>0){
-        for(let i=1;i<=num;i++){
-            console.log(total=total*i);
+/*Esta Funcion corresponde al primer desafio*/
+function factorizar(num){
+    let total = 1;
+    do{
+        num = parseInt(prompt("Ingrese un numero a fatorizar"));
+        
+        if(num>0){
+            for(let i=1;i<=num;i++){
+                console.log(total=total*i);
+            }
+        }else if(num === 0){
+            console.log(total);
         }
-    }else if(num == 0){
-        console.log(total);
     }
+    while(num<0 || isNaN(num))
 }
-while(num<0 || isNaN(num))
+
+function enviar(){
+    // Simulo la toma de info desde la pagina, luego sera reemplazado por el uso del DOM
+    let nombre = "Emanuel Martin";
+    let apellido =  "Zarate";
+    let email = "emanuelmartin@gmail.com"
+    let celular = "11523456789"
+    let comentario = "comentarios ingresados"
+
+    console.log(`Nombre: ${nombre} Apellido: ${apellido} Email: ${email} Celular:${celular} Comentario:${comentario}`);
+}
+
+function carrito(){
+    // Simulo que tengo el carrito con productos e itero para calcular el total
+    // Supongo que tengo 10 productos, esto luego debera ser reemplazado por un array de productos
+    let cantidadProd = 10;
+    // Simulo el precio del producto, esto luego debera ser reemplazado por un objeto
+    let precioProd = 100;
+    // inicializo el total en cero, luego en la medida que se vayan agregando producto se podria calcular el total al momento de agregar el producto con otra funcion
+    
+    let total = 0;
+
+    for(let i=0;i<cantidadProd;i++){
+        if(cantidadProd){
+            total= calcularTotal(total,precioProd); 
+        }
+    }
+
+    console.log(`Total del carrito: $${total}`);
+
+}
+
+function calcularTotal(total, precioProd){
+    return total + precioProd;
+}
