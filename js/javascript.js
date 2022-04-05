7e61b9d9b14735a8014c40344e3c6c481d9797ad
Emanuel Martin Zarate <emanuelmartin@gmail.com>
2022-04-04 23:00:27 -0300

    Entrega de desafio: Interactuar con HTML

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,10 @@
 function enviar(){
-    // Simulo la toma de info desde la pagina, luego sera reemplazado por el uso del DOM
-    let name = "Emanuel Martin";
-    let lastName =  "Zarate";
-    let email = "emanuelmartin@gmail.com"
-    let cellphone = "11523456789"
-    let coments = "comentarios ingresados"
+    const coments = document.getElementById("coments").value;
+    const data = Array.from(document.querySelectorAll('#formContact input')).map((input) =>{
+        return `${input.id} : ${input.value}`;
+    });
 
-    console.log(`name: ${name} Apellido: ${lastName} Email: ${email} Celular:${cellphone} Comentarios:${coments}`);
+    alert(`data: ${data} Comentarios: ${coments}`);
 }
 
 
@@ -25,11 +23,17 @@ class Ecommerce{
         this.products.push(product);
     }
     listProducts(){
-        console.log("Lista de productos")
-        for(const product of this.products)
-            console.log(`Nombre del producto: ${product.name}`);
+        let listCart = document.getElementById("listCart");
+        for(const product of this.products){
+            let contenedor = document.createElement("div");
+            contenedor.className = "column"    
+            contenedor.innerHTML = `<div class="card">
+                                    <h3> Id: ${product.id} </h3>
+                                    <p> Producto: ${product.name}</p>
+                                    <b> $ ${product.price} </b></div>`;
+            listCart.appendChild(contenedor);
+        }
     }
-
 }
 
 
@@ -43,34 +47,12 @@ class Product{
 
 const ecommerce = new Ecommerce(products);
 let product1 = ecommerce.createProduct("1","product1","1000");
+ecommerce.addProduct(product1);
 let product2 = ecommerce.createProduct("2","product2","2000");
-let product3 = ecommerce.createProduct("2","product3","2000");
-let product4 = ecommerce.createProduct("2","product4","2000");
+ecommerce.addProduct(product2);
+let product3 = ecommerce.createProduct("3","product3","2000");
+ecommerce.addProduct(product3);
+let product4 = ecommerce.createProduct("4","product4","2000");
+ecommerce.addProduct(product4);
 
-let buy = true;
-
-while(buy){
-    let productSelected = prompt(`Seleccione producto  a agregar al carrito de compra
-                                 1 - product1
-                                 2 - product2
-                                 3 - product3
-                                 4 - product4
-                                 5 - Listar productos agregados al carrito
-                                 6 - Fin`)
-    if(productSelected === "1"){
-        ecommerce.addProduct(product1); 
-    }else if(productSelected === "2"){
-        ecommerce.addProduct(product2); 
-    }else if(productSelected === "3"){
-        ecommerce.addProduct(product3);
-    }else if(productSelected === "4"){
-        ecommerce.addProduct(product4);
-    }else if(productSelected === "5"){
-        ecommerce.listProducts();
-    }else if(productSelected === "6"){
-        buy = false;
-    }else{
-        alert("Error: Por favor seleccione una de las opciones que se muestran en el menu");
-    }
-
-}
+ecommerce.listProducts()
