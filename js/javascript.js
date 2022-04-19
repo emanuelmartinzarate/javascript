f26c01b3ac1e0ca9c01edb18888539d840069578
Emanuel Martin Zarate <emanuelmartin@gmail.com>
2022-04-18 22:00:13 -0300

    Segunda entrega del proyecto final

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-function enviar(){
+function send(){
     const coments = document.getElementById("coments").value;
     const data = Array.from(document.querySelectorAll('#formContact input')).map((input) =>{
         return `${input.id} : ${input.value}`;
@@ -8,7 +8,45 @@ function enviar(){
 }
 
 
-const products = [];
+const products = [
+    {id:1,title:'product1',description:'description product 1',price:1000,pictureUrl:'picture1',category:'A',stock:'20'},
+    {id:2,title:'product2',description:'description product 2',price:1000,pictureUrl:'picture2',category:'A',stock:'20'},
+    {id:3,title:'product3',description:'description product 3',price:1000,pictureUrl:'picture3',category:'A',stock:'20'},
+    {id:4,title:'product4',description:'description product 4',price:1000,pictureUrl:'picture4',category:'A',stock:'20'}
+]
+
+const cart = []
+
+function addToCart(productId){
+    const product = products.find( product => product.id === productId );
+    //me fijo en el localstorage si ya tengo un carrito
+    const productsInCart = JSON.parse(localStorage.getItem("cart"))
+    if(!productsInCart){
+        cart.push(product)
+        localStorage.setItem("cart",JSON.stringify(cart))
+    }else{
+        productsInCart.push(product)
+        localStorage.setItem("cart",JSON.stringify(productsInCart))
+    }
+}
+
+function showCart(){
+    const productsInCart = JSON.parse(localStorage.getItem("cart"))
+    if(productsInCart){
+        let listProd = document.getElementById("listCart");
+            for(const product of productsInCart){
+                let contenedor = document.createElement("div");
+                contenedor.className = "column"    
+                contenedor.innerHTML = `<div class="card">
+                                        <h3> Id: ${product.id} </h3>
+                                        <p> Producto: ${product.title}</p>
+                                        <b> $ ${product.price} </b><br>
+                                        </div>`;
+                listProd.appendChild(contenedor);
+            }
+    }
+}
+
 
 class Ecommerce{
 
@@ -23,15 +61,17 @@ class Ecommerce{
         this.products.push(product);
     }
     listProducts(){
-        let listCart = document.getElementById("listCart");
+        let listProd = document.getElementById("showProducts");
         for(const product of this.products){
             let contenedor = document.createElement("div");
             contenedor.className = "column"    
             contenedor.innerHTML = `<div class="card">
                                     <h3> Id: ${product.id} </h3>
-                                    <p> Producto: ${product.name}</p>
-                                    <b> $ ${product.price} </b></div>`;
-            listCart.appendChild(contenedor);
+                                    <p> Producto: ${product.title}</p>
+                                    <b> $ ${product.price} </b><br>
+                                    <button onclick="addToCart(${product.id})">Agregar al carrito</button>
+                                    </div>`;
+            listProd.appendChild(contenedor);
         }
     }
 }
@@ -46,13 +86,7 @@ class Product{
 }
 
 const ecommerce = new Ecommerce(products);
-let product1 = ecommerce.createProduct("1","product1","1000");
-ecommerce.addProduct(product1);
-let product2 = ecommerce.createProduct("2","product2","2000");
-ecommerce.addProduct(product2);
-let product3 = ecommerce.createProduct("3","product3","2000");
-ecommerce.addProduct(product3);
-let product4 = ecommerce.createProduct("4","product4","2000");
-ecommerce.addProduct(product4);
 
-ecommerce.listProducts()
+function init(){
+    ecommerce.listProducts()
+}
